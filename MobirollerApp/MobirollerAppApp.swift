110026928f1d09cad28408fc9f3894556c5ed5fd
gokkayaoguz <gokkayaoguz10@gmail.com>
2021-03-04 19:37:51 +0300

    Initial Commit

---
+++ MobirollerApp/MobirollerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobirollerAppApp.swift
+//  MobirollerApp
+//
+//  Created by Baturay Koç on 4.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct MobirollerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
